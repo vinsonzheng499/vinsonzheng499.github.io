de9cff5b7a850a0d9b045a1abaeebd79304456dc
Vinson Zheng <vinsonzheng499@users.noreply.github.com>
2025-03-27 22:58:47 -0500

    Added website structure and about page HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'personal-website';
+  title = 'Vinson Zheng';
 }
