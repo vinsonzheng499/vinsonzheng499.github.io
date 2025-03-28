e9a5e60412fbb59b8adac362f31267fa52eb0a47
Vinson Zheng <vinsonzheng499@gmail.com>
2025-03-27 22:58:47 -0500

    Added website structure and about page HTML

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: '', redirectTo: '/about', pathMatch: 'full' }
+];
